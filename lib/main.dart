cf15dd8e45ac4805092d400f94edf385f6105261
Saarthak-Vijayvargiya-github <saarthak.vijayvargiya@gmail.com>
2022-07-11 15:04:53 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:su_library/models/userData.dart';
 import 'package:su_library/shared/auth.dart';
-import 'package:su_library/student/wrapper.dart';
+import 'package:su_library/shared/wrapper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
